740e508cea8fcf2b9cfea0015fd2ff06442e2035
josh-leebrowne <josh.lv.lee@gmail.com>
2022-12-13 19:19:39 -0600

    week2 done

--- week2/Assignment2/datastructures.py
+++ week2/Assignment2/datastructures.py
@@ -1,18 +1,79 @@
-Data1 = 7, False, "Apple", True, 7, 98.6
+# tuple
+Data1 = (7, False, "Apple", True, 7, 98.6)
 
-Data2 = "July", 4, 8, "Tango", 4.3, "Bingo"
+# set
+Data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"}
 
-Data3 = "A", 7, -1, 3.14, True, 7
+# list
+Data3 = ["A", 7, -1, 3.14, True, 7]
 
-# Data4 =  "name" = "Joe",  "age" = 26,   "active" = True,  "hourly_wage" = 14.75
-
-
-def countDataOne(n):
-    if n <= 1:
-        return n
-
-
-print(countDataOne(Data1))
-
+# dict
+Data4 = {"name": "Joe",
+        "age": 26,
+        "active": True,
+        "hourly_wage": 14.75}
 
 
+# DATA SET 1
+def dataOneTaskOne(Data1):
+    return len(Data1)
+print(dataOneTaskOne(Data1))
+
+def dataOneTaskTwo(Data1):
+    return Data1[3]
+print(dataOneTaskTwo(Data1))
+
+def dataOneTaskThree(Data1):
+    return Data1.count(7)
+print(dataOneTaskThree(Data1))
+
+
+# DATA SET 2
+def dataTwoTaskOne(Data2):
+    return Data2.pop()
+print(dataTwoTaskOne(Data2))
+
+def dataTwoTaskTwo(Data2):
+    return Data2.add('Alpha')
+dataTwoTaskTwo(Data2)
+
+def dataTwoTaskThree(Data2):
+    return Data2
+print(dataTwoTaskThree(Data2))
+
+
+# DATA SET 3
+def dataThreeTaskOne(Data3):
+    return list(reversed(Data3))
+print(dataThreeTaskOne(Data3))
+
+def dataThreeTaskTwo(Data3):
+    Data3.insert(1, 'B')
+    return Data3
+print(dataThreeTaskTwo(Data3))
+
+def dataThreeTaskThree(Data3):
+    Data3.pop()
+    return Data3
+print(dataThreeTaskThree(Data3))
+
+
+# DATA SET 4
+def dataFourTaskOne(Data4):
+    Data4.update({"active": False})
+    return Data4
+print(dataFourTaskOne(Data4))
+
+def dataFourTaskTwo(Data4):
+    Data4["address"] = "123 West Main Street"
+    return Data4
+print(dataFourTaskTwo(Data4))
+
+def dataFourTaskThree(Data4):
+    weeklySalary = (Data4["hourly_wage"] * 40)
+    return weeklySalary
+print(dataFourTaskThree(Data4))
+
+def dataFourTaskFour(Data4):
+    return Data4
+print(dataFourTaskFour(Data4))
--- week2/Project1/project1.py
+++ week2/Project1/project1.py
@@ -1,38 +1,69 @@
-# def loop1():
-#     # Sum the odd numbers between 1 and 20
-#     odd_sum = 0
-#     for i in range(20):
-#         if (i % 2) == 1:
-#             odd_sum += i
-#     return odd_sum
-#
-#
-# def loop2():
-#     # Sum the even numbers between 1 and 20
-#     i = 0
-#     even_sum = 0
-#     while i < 20:
-#         if (i % 2) == 0:
-#             even_sum += i
-#         i += 1
-#     return even_sum
+def loop1():
+    # Sum the odd numbers between 1 and 20
+    odd_sum = 0
+    for i in range(20):
+        if (i % 2) == 1:
+            odd_sum += i
+    return odd_sum
 
 
-# def loop1Rec(num, odd_sum):
-#
-#
-# # Duplicate the loop1 function using recursion
-#
-def loop2Rec(num):
-    if num <= 1:
-        return num
-    elif num >= 1 and (num % 2 == 1):
-        return num + loop2Rec(num - 1)
+def loop2():
+    # Sum the even numbers between 1 and 20
+    i = 0
+    even_sum = 0
+    while i < 20:
+        if (i % 2) == 0:
+            even_sum += i
+        i += 1
+    return even_sum
+
+print(loop1())
+print(loop2())
+
+
+def loop1Rec(num, current, sum = 0):
+    
+    if current == num:
+        return sum
+    elif (current % 2 == 1 and current != num):
+        sum += current
+        return loop1Rec(num, current + 1, sum)
+    else: return loop1Rec(num, current + 1, sum)
 
 
 num = 20
-print(loop2Rec(num))
+current = 0
+sum = 0
+print(loop1Rec(num, current, sum))
+
+
+
+def loop2Rec(num, current, sum = 0):
+    
+    if current == num:
+        return sum
+    elif (current % 2 == 0 and current != num):
+        sum += current
+        return loop2Rec(num, current + 1, sum)
+    else: return loop2Rec(num, current + 1, sum)
+
+
+num = 20
+current = 0
+sum = 0
+print(loop2Rec(num, current, sum))
 
 
 
 
+
+def loop3Rec(num, odd_sum=0, val=0):
+    val = val or 1
+    if (val == num):
+        return odd_sum
+    elif (val % 2 == 1):
+        odd_sum += val
+    val+=1
+    return loop3Rec(num, odd_sum, val)
+
+print(loop3Rec(20))
