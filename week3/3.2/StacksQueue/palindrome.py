193d510fb517d089c3438e0a401403fa89d9c403
josh-leebrowne <josh.lv.lee@gmail.com>
2022-12-18 23:17:03 -0600

    add palindrome

---
+++ week3/3.2/StacksQueue/palindrome.py
@@ -0,0 +1,71 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.dataStack = []
+
+    def pushData(self, data):
+        self.dataStack.append(data)
+    
+    def popData(self):
+        return self.dataStack.pop()
+    
+    def size(self):
+        return len(self.dataStack)
+
+    def isEmpty(self):
+        return True if self.size() == 0 else False
+    
+    def peek(self):
+        return self.dataStack[-1]
+
+class Queue:
+    def __init__(self):
+        self.dataQueue = deque()
+
+    def enqueue(self, data):
+        self.dataQueue.append(data)
+    
+    def dequeue(self):
+        return self.dataQueue.popleft()
+    
+    def peek(self):
+        return self.dataQueue[0]
+    
+    def isEmpty(self):
+        return len(self.dataQueue) == 0
+    
+    def size(self):
+        return len(self.dataQueue)
+
+    def __str__(self):
+        return str(self.dataQueue)
+
+
+
+
+def isPalindrome(x):
+    return x[::-1]
+
+
+stack = Stack()
+stack.pushData('racecar')
+stack.pushData('noon')
+stack.pushData('python')
+stack.pushData('madam')
+print(stack.popData())
+print(stack.size())
+print(stack.isEmpty())
+print(stack.peek())
+print(stack.dataStack)
+
+queue = Queue()
+queue.enqueue('racecar')
+queue.enqueue('noon')
+queue.enqueue('python')
+queue.enqueue('madam')
+print(queue.size())
+print(queue.dequeue())
+print(queue.peek())
+print(queue.isEmpty())
+
