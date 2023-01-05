0d9a9683ebb35e3b2b8346924e8922c71fe2ab91
josh-leebrowne <josh.lv.lee@gmail.com>
2023-01-04 20:57:06 -0600

    project 2 working

--- week4/Project2/hashing.py
+++ week4/Project2/hashing.py
@@ -3,38 +3,45 @@ class HashTable:
         self.size = 10
         self.slots = [None] * self.size
         self.data = [None] * self.size
+
     def hashfunction(self,key):
-        keystr = str(key)
-        hashVal = 0
-        for ch in keystr:
-            hashVal += ord(ch)
+        return key % self.size
 
-        return (hashVal*len(keystr)) % self.size
     def rehash(self,key):
-        keystr = str(key)
-        hashVal = 0
-        for ch in keystr:
-            hashVal += ord(ch)
-
-        return (hashVal*len(keystr)) // self.size
-
+        return key // self.size
+        
     def put(self,key,data):
-        self[key].append(data)
+        hashValue = self.hashfunction(key)
+        if self.slots[hashValue] == None:
+            self.slots[hashValue] = key
+            self.data[hashValue] = data
+        else:
+            hashValue = self.rehash(key)
+            if self.slots[hashValue] == None:
+                self.slots[hashValue] = key
+                self.data[hashValue] = data
+            else:
+                print("Error: Collision")
 
     def get(self,key):
-        return key % len(self)
+        hashValue = self.hashfunction(key)
+        if self.slots[hashValue] == key:
+            return self.data[hashValue]
+        else:
+            hashValue = self.rehash(key)
+            if self.slots[hashValue] == key:
+                return self.data[hashValue]
+            return None
 
     def __getitem__ (self,key):
         return self.get(key)
 
     def __setitem__ (self,key,data):
-        index = hash(key) % len(self)
-        self.data[index] = data
+        self.put(key,data)
 
 
 
 H = HashTable()
-
 H[69] = 'A'
 H[66] = 'B'
 H[80] = 'C'
@@ -45,6 +52,9 @@ H[89] = 'G'
 H[70] = 'H'
 H[12] = 'I'
 
+print(H.slots)
+print(H.data)
+print(H[52])
 
 # Store remaining input data
 # print the slot values
