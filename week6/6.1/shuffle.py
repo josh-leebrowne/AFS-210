4c71ad00b926bd9a071c69d0f7a09dfe7f987965
josh-leebrowne <josh.lv.lee@gmail.com>
2023-01-15 21:27:26 -0600

    6.1 finished

--- week6/6.1/shuffle.py
+++ week6/6.1/shuffle.py
@@ -5,11 +5,12 @@ input = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 print(f"Before Shuffle: {input}")
 
 for i in range(5):
-    for i in range(len(input)-1,0,-1):
+    for i in range(len(input)-1):
             x = random.randint(0,i+1)
             input[i],input[x] = input[x], input[i]
     print(input)
 
+#Time Complexity O(n)
 
 
 
