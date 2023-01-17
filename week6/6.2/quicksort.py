4510025d59a4bf6a591b23c5ad817f60815628e8
josh-leebrowne <josh.lv.lee@gmail.com>
2023-01-16 21:01:50 -0600

    quicksort fin

---
+++ week6/6.2/quicksort.py
@@ -0,0 +1,169 @@
+import time
+import random
+
+def quick_sort(a_list, start, end):
+    # list size is 1 or less (which doesn't make sense)
+    if start >= end:
+        return
+    # Call the partition helper function to split the list into two section 
+    # divided between a pivot point
+    pivot = partitionStart(a_list, start, end)
+    quick_sort(a_list, start, pivot-1)
+    quick_sort(a_list, pivot+1, end)
+        
+def partitionStart(a_list, start, end):
+    return partition(a_list, start, end)
+
+def partition(a_list, start, end):
+    # Select the first element as our pivot point
+    pivot = a_list[start]
+    
+    # Start at the first element past the pivot point
+    low = start + 1
+    high = end
+    while True:
+        # If the current value we're looking at is larger than the pivot
+        # it's in the right place (right side of pivot) and we can move left,
+        # to the next element.
+        # We also need to make sure we haven't surpassed the low pointer, since 
+# that
+#         # indicates we have already moved all the elements to their correct side of
+# the pivot
+        while low <= high and a_list[high] >= pivot:
+            high = high - 1
+        # Opposite process of the one above
+        while low <= high and a_list[low] <= pivot:
+            low = low + 1
+        # We either found a value for both high and low that is out of order
+        # or low is higher than high, in which case we exit the loop
+        if low <= high:
+            # Swap the values
+            a_list[low], a_list[high] = a_list[high], a_list[low]
+            # The loop continues
+        else:
+            # We exit out of the loop
+            break
+    # Swap the values
+    a_list[start], a_list[high] = a_list[high], a_list[start]
+    return high
+print("Quick Sort:")
+#myList = [54,26,93,17,77,31]
+a_list = [x for x in range(1000)]
+random.shuffle(a_list)
+#print(myList)
+start_time = time.time()
+quick_sort(a_list,0,len(a_list)-1)
+end_time = time.time()
+#print()
+#print("Sorted Listed: ")
+# print(a_list)   
+print(f"The execution time is: {end_time-start_time}")
+
+
+def quick_sort(a_list, start, end):
+    if start >= end:
+        return
+    pivot = partitionStart(a_list, start, end)
+    quick_sort(a_list, start, pivot-1)
+    quick_sort(a_list, pivot+1, end)
+
+def partitionStart(a_list, start, end):
+    return partition(a_list, start, end)
+
+#Pivot Start
+def partition(a_list, start, end):
+    pivot = a_list[start]
+    
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and a_list[high] >= pivot:
+            high = high - 1
+        while low <= high and a_list[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            a_list[low], a_list[high] = a_list[high], a_list[low]
+        else:
+            break
+    a_list[start], a_list[high] = a_list[high], a_list[start]
+    return high
+
+
+#Pivot End
+def partition(a_list, start, end):
+    pivot = a_list[end]
+    
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and a_list[high] >= pivot:
+            high = high - 1
+        while low <= high and a_list[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            a_list[low], a_list[high] = a_list[high], a_list[low]
+        else:
+            break
+    a_list[start], a_list[high] = a_list[high], a_list[start]
+    return high
+
+#Middle Pivot
+def partition(a_list, start, end):
+    pivot = a_list[len(a_list)/2]
+    
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and a_list[high] >= pivot:
+            high = high - 1
+        while low <= high and a_list[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            a_list[low], a_list[high] = a_list[high], a_list[low]
+        else:
+            break
+    a_list[start], a_list[high] = a_list[high], a_list[start]
+    return high
+
+#Random Pivot
+def partition(a_list, start, end):
+    pivot = a_list[random.randrange(start, end)]
+    
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and a_list[high] >= pivot:
+            high = high - 1
+        while low <= high and a_list[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            a_list[low], a_list[high] = a_list[high], a_list[low]
+        else:
+            break
+    a_list[start], a_list[high] = a_list[high], a_list[start]
+    return high
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Quick Sort:")
+myList = [x for x in range(1000)]
+random.shuffle(myList)
+start_time = time.time()
+quick_sort(myList,0,len(myList)-1)
+end_time = time.time()
+print(f"The execution time is: {end_time-start_time}")
